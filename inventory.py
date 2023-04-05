e34085e54cd6dee4b0f3f6169821bb92d0e5be9a
Kylie <github.a5cee@kyscott.aleeas.com>
2023-04-05 15:37:43 -0400

    Implement logic for data entry

--- inventory.py
+++ inventory.py
@@ -4,13 +4,18 @@ import config
 from openpyxl import load_workbook
 from splinter import Browser
 
-browser = Browser()
-
 URL = 'https://fedsso.yum.com/idp/startSSO.ping?PartnerSpId=https://yumph.altametrics.com/'
 USER = config.USER
 PASS = config.PASS
 DATE = '04/02/2023'
 FREQ = 'Weekly'
+PATH = pathlib.Path(__file__).parent.resolve().__str__()
+DICT = {'EACH': {'DISK', 'EACH'},
+        'BTL': 'BOTTLE'}
+
+wb = load_workbook(filename = PATH + "/march 27-april 2.xlsx")
+sheet = wb.active
+browser = Browser()
 
 def wait_for_load():
     while browser.find_by_id('loading_layer').visible:
@@ -46,16 +51,42 @@ wait_for_load()
 # Find correct inventory sheet
 browser.find_by_css('.controls').click()
 for link in browser.find_by_tag('li').links.find_by_text(FREQ):
-    if link.visible:
-        link.click()
+    if link.visible: link.click()
 browser.find_by_name('DATE_2').fill(DATE)
 browser.find_by_value('GO').click()
 
 wait_for_load()
 
 for link in browser.find_by_name('openObject'):
-    if link.visible:
-        link.click()
+    if link.visible: link.click()
 
 wait_for_load()
 
+# Cycle through spreadsheet and enter data
+for row in sheet.iter_rows(min_row=6, max_row=7, min_col=0, max_col=8):
+    skip = False
+    for cell in row:
+        match cell.column_letter:
+            case "A":
+                if cell.value is None: 
+                    skip = True
+                    break
+                itemCode = cell.value.strip()
+            case "B":
+                itemDesc = cell.value.strip()
+            case "C":
+                itemUnit = cell.value.strip()
+                itemUnit = DICT[itemUnit] if itemUnit in DICT else itemUnit
+            case "H":
+                if cell.value is None: 
+                    skip = True
+                    break
+                itemCount = cell.value
+    if skip: continue
+    
+    browser.find_by_id('INV_ACC_DETAIL_tbl_filter').find_by_tag('input').fill(itemCode)
+    for td in browser.find_by_id('INV_ACC_DETAIL_tbl').find_by_tag('td'):
+        if td.text in itemUnit: 
+            prev.find_by_tag('input').fill(itemCount)
+            break
+        prev = td

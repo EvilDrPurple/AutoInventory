effec2180dac1703317d01ff35bce77d2bfe3a08
Kylie <github.a5cee@kyscott.aleeas.com>
2023-04-23 23:01:05 -0400

    Create UserCancelledExcetion class

--- exceptions.py
+++ exceptions.py
@@ -2,3 +2,7 @@ class LoginFailedException(Exception):
     def __init__(self, user, message='Login failed for user '):
         self.user = user
         self.message = f"{message} {self.user}\n"
+
+class UserCancelledException(Exception):
+    def __init__(self, message='Process cancelled by user\n'):
+        self.message = message

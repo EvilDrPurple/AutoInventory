62fcb688ad1f854511f82d240dc1d8027283694e
Kylie <github.a5cee@kyscott.aleeas.com>
2023-05-28 18:59:47 -0400

    Change setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'autoinventory.py',
     '--onefile',
-    '-n AutoInventory',
-    '-ipizza_hut_logo.png'
-    #'--add-data=chromedriver.exe;chromedriver.exe'
+    '-nAutoInventory',
+    '-ipizza_hut_logo.ico'
 ])
